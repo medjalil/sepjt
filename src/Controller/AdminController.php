cf5d818167b6330c2c70bf66c6d710149c45d865
medjalil <med.abdeljalil@gmail.com>
2020-07-10 19:28:52 +0100

    update user password encoder

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdminController extends EasyAdminController {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    protected function persistUserEntity($user) {
+        $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()));
+        parent::persistEntity($user);
+    }
+
+    protected function updateUserEntity($user) {
+        $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()));
+
+        parent::updateEntity($user);
+    }
 
 }
